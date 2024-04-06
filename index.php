5382b6b6c123f74c5a4d20376def035c5b1dc85c
Janith Chathuranga <127806197+janithScript@users.noreply.github.com>
2024-04-06 13:49:54 +0530

    update in index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <title>How to generate PDF in PHP dynamically by using TCPDF</title>
+  <title>Generate PDF in PHP dynamically by using TCPDF</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
